b1435a750061b40c6af46041cf6a335a43299e7c
Chenghan Sun <sunchenghan1995@gmail.com>
2020-01-27 21:04:48 -0800

    fix bug

--- code/02_descriptor.py
+++ code/02_descriptor.py
@@ -1,45 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-'''
+"""
 This file:
     linked with: .traj file @ ../tests/traj_folder/ and finger_f90 @ ../src/
     output: fingerprints file @ ../tests/fps_path/
-'''
+"""
 
-import os, math, argparse, hashlib, subprocess, sys
+import os
+import math
+import subprocess
 import numpy as np
-import pickle, datetime
-from ase import Atoms, Atom, units
+import pickle
+import datetime
 from ase.io.trajectory import Trajectory
-from amp.utilities import hash_images, get_hash
+from amp.utilities import get_hash
 import shutil
+
 
 def traj_info(atoms):
     """ Summary:
             function generates information of the atomistic system
             files written out @ given directory
     """
-    f=open('cell_matrix', 'w+')
+    f = open('cell_matrix', 'w+')
     f.write("%8.6f %8.6f %8.6f\n" % ( atoms.cell[0][0], atoms.cell[0][1], atoms.cell[0][2]))
     f.write("%8.6f %8.6f %8.6f\n" % ( atoms.cell[1][0], atoms.cell[1][1], atoms.cell[1][2]))
     f.write("%8.6f %8.6f %8.6f\n" % ( atoms.cell[2][0], atoms.cell[2][1], atoms.cell[2][2]))
     f.close()
 
-    f=open('positions', 'w+')
+    f = open('positions', 'w+')
     for atom in atoms.get_scaled_positions():
         f.write("%8.6f %8.6f %8.6f\n" % ( atom[0], atom[1], atom[2] ))
     f.close()
 
-    f=open('elements', 'w+')
+    f = open('elements', 'w+')
     for atom in atoms.get_chemical_symbols():
         f.write(atom + '\n')
     f.close()
 
-    symbols=sorted(set(atoms.get_chemical_symbols()))
-    f=open('element_alist', 'w+')
+    symbols = sorted(set(atoms.get_chemical_symbols()))
+    f = open('element_alist', 'w+')
     for atom in symbols:
         f.write(atom + '\n')
     f.close()
+
 
 def fp_generator(trajfile, Rc, calc_primes, fortran_path):
     """ Summary:
@@ -54,9 +58,9 @@ def fp_generator(trajfile, Rc, calc_primes, fortran_path):
             writeout fingerprints files to the given folder
     """
     # generate descriptor path
-    path_prime='amp-fingerprint-primes.ampdb/loose/'
-    path_finger='amp-fingerprints.ampdb/loose/'
-    path_neighbor='amp-neighborlists.ampdb/loose/'
+    path_prime = 'amp-fingerprint-primes.ampdb/loose/'
+    path_finger = 'amp-fingerprints.ampdb/loose/'
+    path_neighbor = 'amp-neighborlists.ampdb/loose/'
 
     # check if paths exist
     if not os.path.exists(path_prime):
@@ -77,47 +81,47 @@ def fp_generator(trajfile, Rc, calc_primes, fortran_path):
         else:
             print("Fingerprinting " + hash + " " + str(datetime.datetime.now()))
         # write out traj information
-        traj_info(atoms) #$, 'cell_matrix', 'positions', 'elementlist', 'element_type')
+        traj_info(atoms)  # $, 'cell_matrix', 'positions', 'elementlist', 'element_type')
 
         # call the fortran acceleration code
-        symbols=sorted(set(atoms.get_chemical_symbols()))
+        symbols = sorted(set(atoms.get_chemical_symbols()))
         print(symbols)
-        #command = "/Users/furinkazan/metal@zeo_codes/finger_f90" + " " + str(len(atoms)) + " " + str(len(symbols)) + " " + str(Rc) + " " + str(calc_primes)
+        # command = "/Users/furinkazan/metal@zeo_codes/finger_f90" + " " + str(len(atoms)) + " " + str(len(symbols)) + " " + str(Rc) + " " + str(calc_primes)
         command = fortran_path + " " + str(len(atoms)) + " " + str(len(symbols)) + " " + str(Rc) + " " + str(calc_primes)
-        print (command)
+        print(command)
 
-        #subprocess.call( "/Users/furinkazan/metal@zeo_codes/finger_f90" + " " + str(len(atoms)) + " " + str(len(symbols)) + " " + str(Rc) + " " + str(calc_primes), shell=True)
+        # subprocess.call( "/Users/furinkazan/metal@zeo_codes/finger_f90" + " " + str(len(atoms)) + " " + str(len(symbols)) + " " + str(Rc) + " " + str(calc_primes), shell=True)
         subprocess.call(fortran_path + " " + str(len(atoms)) + " " + str(len(symbols)) + " " + str(Rc) + " " + str(calc_primes), shell=True)
         elem_list = []
         neighbors_list = []
         fingers = []
         if os.path.isfile('elements'):
-            file=open('elements', 'r')
+            file = open('elements', 'r')
             for line in file:
                 elem_list.append(line.strip())
                 neighbors_list.append([[], []])
-                fingers.append( (line.strip(), ) )
+                fingers.append((line.strip(),))
         else:
             print("elements file not exist")
 
-        natoms=len(elem_list)
-        ntypes=len(set(elem_list))
+        natoms = len(elem_list)
+        ntypes = len(set(elem_list))
 
         # file neighbors and fingerprints generated from Fortran module
         if os.path.isfile('neighbors'):
             file = open('neighbors', 'r')
             for line in file:
-                line_items=line.split()
-                aa=int(line_items[0])-1
-                bb=int(line_items[1])-1
-                offset=[ int(line_items[2]), int(line_items[3]), int(line_items[4]) ]
+                line_items = line.split()
+                aa = int(line_items[0])-1
+                bb = int(line_items[1])-1
+                offset = [int(line_items[2]), int(line_items[3]), int(line_items[4])]
                 neighbors_list[aa][0].append(bb)
                 neighbors_list[aa][1].append(offset)
             file.close()
 
         tup_list=[]
         for aa in range(natoms):
-            tup1=( np.array(neighbors_list[aa][0]), np.array(neighbors_list[aa][1]) )
+            tup1 = (np.array(neighbors_list[aa][0]), np.array(neighbors_list[aa][1]))
             tup_list.append(tup1)
 
         f = open(path_neighbor + hash, 'wb')
@@ -161,6 +165,7 @@ def fp_generator(trajfile, Rc, calc_primes, fortran_path):
         f = open(path_prime + hash, 'wb')
         pickle.dump(primes, f)
         f.close()
+
 
 if __name__ == "__main__":
     trajfile = "../traj_folder/all.traj"
