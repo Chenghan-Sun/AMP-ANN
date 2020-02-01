38838274098fc0037ebdc1a44f449b195fd3c790
Chenghan Sun <sunchenghan1995@gmail.com>
2020-01-31 17:38:56 -0800

    separate training classes

--- src/training_utils.py
+++ src/training_utils.py
@@ -12,301 +12,28 @@ This file:
 
 """
 
-import os
 import sys
 from ase import io
 import numpy as np
-import shutil
 
-# Database Module
-from ase.db import connect
-
-# Math Module
-from random import shuffle
 from itertools import chain
-from scipy.optimize import basinhopping
 
 # AMP Module
 from amp import Amp
 from amp.model.neuralnetwork import NeuralNetwork
-from amp.descriptor.gaussian import Gaussian, make_symmetry_functions
-from amp.utilities import hash_images, get_hash
-from amp.model.neuralnetwork import NodePlot
 from amp.regression import Regressor
 from amp.model import LossFunction
 
-# AMP analysis
-from amp.descriptor.analysis import FingerprintPlot
-from amp.analysis import plot_parity
-from amp.analysis import plot_sensitivity
 from amp.analysis import plot_convergence
 
 # Visual
 import matplotlib.pyplot as plt
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-from ase.visualize import view
 
 # Sklearn module
 sys.path.insert(0, "/usr/local/lib/python3.7/site-packages/")
 from sklearn.metrics import mean_squared_error
 from sklearn import preprocessing
-
-
-class DatabaseTools:
-    """ Summary:
-            ASE Database tools used for sampling data from raw DFT calculations
-        init:
-            specify directory path of database, database name
-            default all training-validation .traj file names
-        TODO: #1 add read-in vasprun.xml file functionality
-    """
-    def __init__(self, db_path, db_name, dataset='all.traj',
-                 training_traj='trainset.traj', validation_traj='validset.traj'):
-        self.path = db_path
-        self.name = db_name
-        self.dataset = dataset
-        self.training_traj = training_traj
-        self.validation_traj = validation_traj
-
-    def db_selector(self, **kwargs):
-        """ Summary:
-                Extract traj list of atom configurations from database
-            Parameters:
-                kwargs: dictionary of db search key words
-            Returns:
-                traj: atoms trajectory list
-        """
-        traj = []
-        db = connect(self.path + self.name)
-        dict_v = [v for k, v in kwargs.items()]
-        dict_v = ", ".join(dict_v)  # list to string
-        for row in db.select(dict_v):
-            atoms = db.get_atoms(id=row.id)
-            traj.append(atoms)
-        return traj
-
-    @staticmethod
-    def train_test_split(traj, training_weight, shuffle_mode=True):
-        """ Summary:
-                split selected trajs into train and test sets
-            Parameters:
-                traj: traj file as a list from db_selector function
-                training_weight: weight percentage of train-test split
-                shuffle_mode: = 0 or 1 randomly selection or dummy selection
-            Returns:
-                length of traj file, training traj, valid traj
-        """
-        if shuffle_mode:  # shuffle the dataset
-            shuffle(traj)
-        else:
-            pass
-
-        len_traj = len(traj)
-        len_train = round(training_weight*len(traj))
-        traj_train = traj[:len_train]
-        traj_valid = traj[len_train:]
-        return len_traj, traj_train, traj_valid
-
-
-class FpsAnalysisTools:
-    """ Summary:
-            Some notes about the fp data structure:
-            For 7nps@Zeo system
-            The key of fp --> each of the atoms, 43 in total
-                the hash of fp: 0 --> the element type of atom e.g. oxygen
-                1 --> each fp values, 36 in total, refering to the Gaussian data
-            if fp = descriptor.fingerprints[hash]:
-                fp gives # of images, each image contains list of all 43 elements, each element
-                has 36 fp values, which could be tested by:
-                fp = descriptor.fingerprints[hash][atom_index][1]
-                print(fp, len(fp)))
-            The number of fps caculation rule:
-                G2: 4 eta-parameters X 3 different type of atoms = 12 fps
-                G4: 1 eta-parameter X 2 zeta-parameter X 2 gamma-parameter X 6 (A3_2) types of
-                2-atom interactions = 24 fps
-                --> all together 36 fps
-    """
-    def __init__(self, metal, mode, ):
-        """ Parameters:
-                metal: type of metal nanocluster
-                mode:
-                    =0: only the metal nanocluster
-                    =1: netal nanocluster with zeolite framework
-        """
-        self.metal = metal
-        self.mode = mode
-
-    def descriptor_generator(self, G2_etas, G4_etas, G4_zetas, G4_gammas):
-        """ Summary:
-                Use this function solely for Amp calculator set-up
-            Parameters:
-                #1-4 Use Gaussian Functions as fingerprints kernel by changing
-                the parameters of G2 and G4
-            Returns:
-                the descriptor: <amp.descriptor.gaussian.Gaussian object at 0x1013116a0>
-                G data structure：
-                example:
-                    [{'type': 'G2', 'element': 'Pt', 'eta': 0.05}, {'type': 'G2', 'element': 'Pt', 'eta': 4.0},
-                    {'type': 'G2', 'element': 'Pt', 'eta': 20.0}, {'type': 'G2', 'element': 'Pt', 'eta': 80.0},
-                    {'type': 'G4', 'elements': ['Pt', 'Pt'], 'eta': 0.005, 'gamma': 1.0, 'zeta': 1.0},
-                    {'type': 'G4', 'elements': ['Pt', 'Pt'], 'eta': 0.005, 'gamma': -1.0, 'zeta': 1.0},
-                    {'type': 'G4', 'elements': ['Pt', 'Pt'], 'eta': 0.005, 'gamma': 1.0, 'zeta': 4.0},
-                    {'type': 'G4', 'elements': ['Pt', 'Pt'], 'eta': 0.005, 'gamma': -1.0, 'zeta': 4.0}]
-        """
-        elements = [self.metal]
-        if self.mode == 0:
-            pass
-        elif self.mode == 1:
-            elements.append('Si')
-            elements.append('O')
-        else:
-            raise Exception('mode should be 0 or 1. The value of mode was: {}'.format(self.mode))
-
-        G = make_symmetry_functions(elements=elements, type='G2',
-                                    etas=G2_etas)
-        G += make_symmetry_functions(elements=elements, type='G4',
-                                     etas=G4_etas,
-                                     zetas=G4_zetas,
-                                     gammas=G4_gammas)
-        my_Gs = {self.metal: G}
-        if self.mode == 0:
-            pass
-        elif self.mode == 1:
-            my_Gs.update({'Si': G, 'O': G})
-        descriptor = Gaussian(Gs=my_Gs)
-        return descriptor, G
-
-    @staticmethod
-    def calc_fp(descriptor, images):
-        """ Summary:
-                transform .traj images to hash images
-            Parameters:
-                descriptor: from fcn --> descriptor_generator
-                images: by ase.io.read() .traj file
-            Returns:
-                descriptor: descriptor with calculated fps
-                images: hashed images
-        """
-        images = hash_images(images)
-        descriptor.calculate_fingerprints(images)
-        return descriptor, images
-
-    def fp_values_extractor(self, atom_index, G, hash, descriptor, fig_name="", fig_flag=False):
-        """ Summary:
-                Extract the fingerprint value from hased images based on descriptor
-                a hash = a image = an atomistic system
-            Parameters:
-                atom_index: fp of which number of atom in the system
-                    e.g. atom_index=0 is the first O element
-                G: symmetry functions, return from fcn --> descriptor_generator
-                hash: receive the arg from emurating the images from fcn --> calc_fp
-                descriptor from fcn --> calc_fp
-                figure name: given name of the plotfile, default to ""
-                fig_flag: falg for if user wants a plot
-            Returns:
-                printout information about the fingerprints of a atom in a hash image
-                scatter plot of the fingerprint values
-        """
-        fp = descriptor.fingerprints[hash][atom_index][1] # unpack the hash images
-        for j, val in enumerate(fp):
-            g = G[j]
-            if g['type'] == 'G2':
-                ele = g['element']
-            elif g['type'] == 'G4':
-                ele = g['elements']
-            else:
-                raise Exception("No such symmetry function implemented")
-            print(j, g['type'], ele, g['eta'], val)
-
-            if fig_flag == True:
-                if self.metal in ele:
-                    my_marker = 'o' # o refers element with metal
-                else:
-                    my_marker = 'x'
-                plt.plot(j, val, my_marker)
-                plt.savefig(fig_name)
-
-    @staticmethod
-    def get_extreme_value_fp(descriptor, images):
-        """ Summary:
-                Extract the maximum and minimum values of all the fingerprints generated
-            Parameters:
-                enumerating the images returned by fcn --> calc_fp
-            Returns:
-                printout maximum and minimum values of fp
-        """
-        for index, hash in enumerate(images.keys()):
-            fp = descriptor.fingerprints[hash]  # [0][1] all the information of fp
-            max_all_image_fp_list = []  # max fp value of all images
-            min_all_image_fp_list = []  # min fp value of all images
-            for i in fp:
-                # i = each atom and its 36 fp values
-                # print(i[1]) #5(image)X43(row)X36(column)
-                max_i_fp = max(i[1])  # i[0] --> atom type, i[1] --> 36 fp values
-                min_i_fp = min(i[1])
-                max_all_image_fp_list.append(max_i_fp)
-                min_all_image_fp_list.append(min_i_fp)
-
-            max_fp = max(max_all_image_fp_list)
-            min_fp = min(min_all_image_fp_list)
-            print("The maximum value of the fp is " + str(max_fp))
-            print("The minimum value of the fp is " + str(min_fp))
-        return max_fp, min_fp
-
-    def fp_barplot(self, atom_index, G, hash, descriptor, fig_name, title):
-        """ Summary:
-                Makes a barplot of the fingerprints for a certain atom
-            Parameters:
-                atom_index: fp of which number of atom in the system
-                    e.g. atom_index=0 is the first O element
-                G: symmetry functions, return from fcn --> descriptor_generator
-                hash: receive the arg from emurating the images from fcn --> calc_fp
-                descriptor from fcn --> calc_fp
-                figure name: given name of the plotfile
-                title: title of each sub-polt
-            Returns:
-                barplot of the fingerprint components for a certain atom
-        """
-        fp = descriptor.fingerprints[hash][atom_index][1]  # list
-        fig, ax = plt.subplots()
-        G2_val = []
-        G4_val = []
-        metal_index_list = []  # lists indicate indices of zeo or metal
-        zeo_index_list = []
-        metal_ele_list = []  # lists indicate zeo or metal
-        zeo_ele_list = []
-
-        for j, val in enumerate(fp):
-            g = G[j]
-            if g['type'] == 'G2':
-                G2_val.append(val)
-                ele = g['element']
-            elif g['type'] == 'G4':
-                G4_val.append(val)
-                ele = g['elements']
-
-            if self.mode == 0:
-                metal_index_list.append(j)
-                metal_ele_list.append(val)
-            elif self.mode == 1:
-                if self.metal in ele:
-                    metal_index_list.append(j)
-                    metal_ele_list.append(val)
-                else:
-                    zeo_index_list.append(j)
-                    zeo_ele_list.append(val)
-
-        ax.bar(range(len(G2_val)), G2_val, label="G1")  # set the x-y axis
-        ax.bar(np.arange(len(G4_val)) + len(G2_val), G4_val, label="G2")
-        ax.set_title(title, fontsize=14)
-        ax.set_ylim(0., 12.)
-        ax.set_xlabel('Index of fingerprints', fontsize=14)
-        ax.set_ylabel('Value of fingerprints', fontsize=14)
-        ax.scatter(metal_index_list, metal_ele_list, marker='o', color='purple', label='metal atom')
-        if self.mode == 1:
-            ax.scatter(zeo_index_list, zeo_ele_list, marker='x', color='purple', label='only zeo atoms')
-        ax.legend(fontsize=14)
-        fig.savefig(fig_name)
 
 
 class TrainTools:
@@ -381,7 +108,6 @@ class TrainTools:
             calc.train(images=training_traj)
         else:
             print("Training NOT Start")
-        return calc
 
     @staticmethod
     def get_dft_energy(training_traj, validation_traj, rel_option):
@@ -641,42 +367,3 @@ class TrainTools:
         plt.legend()
         plt.savefig('./' + fig_title + '.png')
         return rms_train, rms_valid
-
-
-class ModelAnalysis:
-    """ implemented convergence and parameters analysis for Atomistic Machine Learning task
-    """
-    def __init__(self, default, logpath, logfile='./amp-log.txt'):
-        self.logfile = logfile
-        self.default = default
-        self.logpath = logpath
-
-    def conv_plot_default(self, label):
-        """ convergence plot using default module
-            Inputs: label (e.g. NN architecture 5x5)
-            Outputs: .png convergence behavior plot
-        """
-        if self.default is True:
-            logfile = self.logpath+'/'+self.logfile
-            plot_convergence(logfile, plotfile='convergence.png')
-        elif self.default is False:
-            logfile = self.logpath+'/amp-log-'+label+'.txt'
-            plot_convergence(logfile, plotfile='convergence-'+label+'.png')
-
-    @staticmethod
-    def extract_logfile(logfile):
-        """ Reads the log file from the training process, returning the relevant
-        parameters
-            Inputs: amp-log.txt file from a training circle
-            Outputs:
-        """
-        data = {}
-
-        with open(logfile, 'r') as f:
-            lines = f.read().splitlines()
-            print(len(lines))
-        print('file opened')
-
-        """
-        TODO: temporarily for QE Results:
-        """
