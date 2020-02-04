3d1491f881241e965fe743a07ef7a19a0d9468a5
Chenghan Sun <sunchenghan1995@gmail.com>
2020-02-04 15:00:45 -0800

    update get_forces, plots

--- src/training_utils.py
+++ src/training_utils.py
@@ -3,15 +3,15 @@
 """
 This file:
     Repo for utilities of Atomistic Machine Learning Project
-    implemented classes:
-        `TrainTools:
-
+    implemented classes: TrainTools
+    1. Learning Algorithm
+    2. E/F calculations
+    3. Plotting
 """
 
 import sys
 from ase import io
 import numpy as np
-
 from itertools import chain
 
 # AMP Module
@@ -37,6 +37,7 @@ class TrainTools:
     Note:
         Use coupling with Fp_values_extractor method for generating descriptor.
     """
+
     def __init__(self, descriptor, path, force_option=True, training_traj='trainset.traj',
                  validation_traj='validset.traj'):
         """
@@ -110,7 +111,7 @@ class TrainTools:
         else:
             raise Exception("Training NOT Start")
 
-    def get_energies(self, calc, training_traj, validation_traj, set_calc=False, rel_option=False):
+    def get_energy_dict(self, calc, training_traj, validation_traj, set_calc=True, rel_option=False):
         """
         Summary:
             function to collect DFT / ML energies
@@ -165,8 +166,7 @@ class TrainTools:
                 self.e_valid_dict[key] = val
             return self.e_train_dict, self.e_valid_dict
 
-    def get_forces(self, calc, training_traj, validation_traj, set_calc=False, rel_option=False,
-                   norm_option=False, normalize=False):
+    def get_forces_dict(self, calc, training_traj, validation_traj, set_calc=True):
         """
         Summary:
             function to collect DFT / ML forces
@@ -175,9 +175,6 @@ class TrainTools:
             training_traj: training set
             validation_traj: validation set
             set_calc: flag for setting up trained calculator, default to be just DFT forces
-            rel_option: flag for relative forces
-            norm_option: flag for total forces
-            normalize: flag for normalized forces
         Returns:
 
         """
@@ -205,12 +202,22 @@ class TrainTools:
                 self.f_valid_dict[index] = f_ml
             else:
                 raise Exception("Get Forces: validation set calculator set-up is not specified!")
+        return self.f_train_dict, self.f_valid_dict
 
-        # decompose forces into X-Y-Z axises
+    def force_decompose(self, rel_option=False, norm_option=False, normalize=False):
+        """
+        Summary:
+            Decompose forces into X-Y-Z axises
+        TODO:
+            rel_option: flag for relative forces
+            norm_option: flag for total forces
+            normalize: flag for normalized forces
+        """
         x_train_force_list = []
         y_train_force_list = []
         z_train_force_list = []
-        merged_f_train_dict_val = list(chain(*self.f_train_dict.values()))
+        f_train_dict_val = [val for val in self.f_train_dict.values()]
+        merged_f_train_dict_val = list(chain(*f_train_dict_val))
         for i in merged_f_train_dict_val:
             x_train_force_list.append(i[0])
             y_train_force_list.append(i[1])
@@ -219,201 +226,99 @@ class TrainTools:
         x_valid_force_list = []
         y_valid_force_list = []
         z_valid_force_list = []
-        merged_f_dft_valid = list(chain(*f_dft_validation))
-        for i in merged_f_dft_valid:
+        f_valid_dict_val = [val for val in self.f_valid_dict.values()]
+        merged_f_valid_dict_val = list(chain(*f_valid_dict_val))
+        for i in merged_f_valid_dict_val:
             x_valid_force_list.append(i[0])
             y_valid_force_list.append(i[1])
             z_valid_force_list.append(i[2])
 
-        # normalize the decomposed forces
-        if normalize is True:
+        # normalize each decomposed forces
+        if not normalize:  # default normalize=False
             x_nor_train_force_list = preprocessing.normalize(x_train_force_list, norm='l2')
             y_nor_train_force_list = preprocessing.normalize(y_train_force_list, norm='l2')
             z_nor_train_force_list = preprocessing.normalize(z_train_force_list, norm='l2')
             x_nor_valid_force_list = preprocessing.normalize(x_valid_force_list, norm='l2')
             y_nor_valid_force_list = preprocessing.normalize(y_valid_force_list, norm='l2')
             z_nor_valid_force_list = preprocessing.normalize(z_valid_force_list, norm='l2')
+            return x_nor_train_force_list, x_nor_valid_force_list, y_nor_train_force_list, \
+                   y_nor_valid_force_list, z_nor_train_force_list, z_nor_valid_force_list
 
-        # translate to total force
-        if norm_option is True:
-            f_dft_train = [np.linalg.norm(forces) for forces in f_dft_train]
-            f_dft_validation = [np.linalg.norm(forces) for forces in f_dft_validation]
-            # option to get relative forces
-            f_dft = np.concatenate((f_dft_train, f_dft_validation))
-            rel_f_dft_tra = f_dft_train - min(f_dft)
-            rel_f_dft_val = f_dft_validation - min(f_dft)
-            return f_dft_train, f_dft_validation, rel_f_dft_tra, rel_f_dft_val
-
-        elif norm_option is False and rel_option is True:
-            f_x_dft = np.concatenate((x_train_force_list, x_valid_force_list))
-            rel_f_x_dft_train = x_train_force_list - min(f_x_dft)
-            rel_f_x_dft_valid = x_valid_force_list - min(f_x_dft)
-            f_y_dft = np.concatenate((y_train_force_list, y_valid_force_list))
-            rel_f_y_dft_train = y_train_force_list - min(f_y_dft)
-            rel_f_y_dft_valid = y_valid_force_list - min(f_y_dft)
-            f_z_dft = np.concatenate((z_train_force_list, z_valid_force_list))
-            rel_f_z_dft_train = z_train_force_list - min(f_z_dft)
-            rel_f_z_dft_valid = z_valid_force_list - min(f_z_dft)
-            return (rel_f_x_dft_train, rel_f_x_dft_valid, rel_f_y_dft_train,
-                    rel_f_y_dft_valid, rel_f_z_dft_train, rel_f_z_dft_valid)
-
-        elif norm_option is False and rel_option is False and normalize is False:
-            return (x_train_force_list, x_valid_force_list, y_train_force_list,
-                    y_valid_force_list, z_train_force_list, z_valid_force_list)
-
-        elif norm_option is False and rel_option is False and normalize is True:
-            return (x_nor_train_force_list, x_nor_valid_force_list, y_nor_train_force_list,
-                    y_nor_valid_force_list, z_nor_train_force_list, z_nor_valid_force_list)
-
-
-    def get_neuralnet_force(self, calc, training_traj, validation_traj, rel_option, norm_option, normalize_option):
-        """ Inputs: trained AMP calculator
-            Outputs: raw and relative AMP forces
-            TODO: dictionary replacing list
-        """
-        if self.force_option is False:
-            raise ValueError('Force_option is not turned on!')
         else:
-            pass
-        # assign AMP calculator
-        f_amp_train = []
-        f_amp_validation = []
-        for atoms in training_traj:
-            atoms.set_calculator(calc)
-            f_train = atoms.get_forces()
-            f_amp_train.append(f_train)
-        for atoms in validation_traj:
-            atoms.set_calculator(calc)
-            f_valid = atoms.get_forces()
-            f_amp_validation.append(f_valid)
+            return x_train_force_list, x_valid_force_list, y_train_force_list, y_valid_force_list, \
+                   z_train_force_list, z_valid_force_list
 
-        # decompose forces into X-Y-Z axises
-        x_train_force_list = []
-        y_train_force_list = []
-        z_train_force_list = []
-        merged_f_amp_train = list(chain(*f_amp_train))
-        for i in merged_f_amp_train:
-            x_train_force_list.append(i[0])
-            y_train_force_list.append(i[1])
-            z_train_force_list.append(i[2])
-
-        x_valid_force_list = []
-        y_valid_force_list = []
-        z_valid_force_list = []
-        merged_f_amp_valid = list(chain(*f_amp_validation))
-        for i in merged_f_amp_valid:
-            x_valid_force_list.append(i[0])
-            y_valid_force_list.append(i[1])
-            z_valid_force_list.append(i[2])
-
-        # normalize the decomposed forces
-        if normalize_option == True:
-            x_nor_train_force_list = preprocessing.normalize(x_train_force_list, norm='l2')
-            y_nor_train_force_list = preprocessing.normalize(y_train_force_list, norm='l2')
-            z_nor_train_force_list = preprocessing.normalize(z_train_force_list, norm='l2')
-            x_nor_valid_force_list = preprocessing.normalize(x_valid_force_list, norm='l2')
-            y_nor_valid_force_list = preprocessing.normalize(y_valid_force_list, norm='l2')
-            z_nor_valid_force_list = preprocessing.normalize(z_valid_force_list, norm='l2')
-
-        # translate to total force
-        if norm_option == True:
-            f_amp_train = [np.linalg.norm(forces) for forces in f_amp_train]
-            f_amp_validation = [np.linalg.norm(forces) for forces in f_amp_validation]
-            # option to get relative forces
-            f_amp = np.concatenate((f_amp_train, f_amp_validation))
-            rel_f_amp_tra = f_amp_train - min(f_amp)
-            rel_f_amp_val = f_amp_validation - min(f_amp)
-            return f_amp_train, f_amp_validation, rel_f_amp_tra, rel_f_amp_val
-
-        elif (norm_option == False and rel_option == True):
-            f_x_amp = np.concatenate((x_train_force_list, x_valid_force_list))
-            rel_f_x_amp_train = x_train_force_list - min(f_x_amp)
-            rel_f_x_amp_valid = x_valid_force_list - min(f_x_amp)
-            f_y_amp = np.concatenate((y_train_force_list, y_valid_force_list))
-            rel_f_y_amp_train = y_train_force_list - min(f_y_amp)
-            rel_f_y_amp_valid = y_valid_force_list - min(f_y_amp)
-            f_z_amp = np.concatenate((z_train_force_list, z_valid_force_list))
-            rel_f_z_amp_train = z_train_force_list - min(f_z_amp)
-            rel_f_z_amp_valid = z_valid_force_list - min(f_z_amp)
-            return (rel_f_x_amp_train, rel_f_x_amp_valid, rel_f_y_amp_train,
-                    rel_f_y_amp_valid, rel_f_z_amp_train, rel_f_z_amp_valid)
-
-        elif (norm_option == False and rel_option == False and normalize_option == False):
-            return (x_train_force_list, x_valid_force_list, y_train_force_list,
-                    y_valid_force_list, z_train_force_list, z_valid_force_list)
-
-        elif (norm_option == False and rel_option == False and normalize_option == True):
-            return (x_nor_train_force_list, x_nor_valid_force_list, y_nor_train_force_list,
-                    y_nor_valid_force_list, z_nor_train_force_list, z_nor_valid_force_list)
-
-    def fitting_energy_plot(self, e_dft_train, e_dft_validation, e_amp_train, e_amp_validation, fig_title):
-        """ Inputs: train-valid dft/amp raw/rel energies
-            Outputs: Figure 1: fitting results
-                        blue circles: raw DFT dataset
-                        black line: fitted values
-                        orange circles: predicted values
-                     Figure 2: Histogram of energy sampling
+    @staticmethod
+    def fitting_energy_plot(e_dft_train, e_dft_valid, e_amp_train, e_amp_valid, fig_title):
+        """
+        Summary:
+            train versus validation plot for energies
+        Returns:
+            Sub - Figure 1: fitting results
+                blue circles: raw DFT dataset
+                black line: fitted values
+                orange circles: predicted values
+            Sub - Figure 2: Histogram of energy sampling
         """
         plt.figure(figsize=(6, 8))
         plt.subplot(2, 1, 1)
         plt.plot(e_dft_train, e_dft_train, '-k')
         plt.plot(e_dft_train, e_amp_train, 'o', fillstyle='full', color=colors[0],
                  label='training', markeredgecolor='k')
-        plt.plot(e_dft_validation, e_amp_validation, 'o', fillstyle='full', color=colors[1],
+        plt.plot(e_dft_valid, e_amp_valid, 'o', fillstyle='full', color=colors[1],
                  label='validation', markeredgecolor='k')
         plt.legend()
-        plt.xlabel('DFT energy, eV')
-        plt.ylabel('ML-FF energy, eV')
-        rms_valid = np.sqrt(mean_squared_error(e_dft_validation, e_amp_validation))
+        plt.xlabel('DFT energies, eV')
+        plt.ylabel('ML-FF energies, eV')
+        rms_valid = np.sqrt(mean_squared_error(e_dft_valid, e_amp_valid))
         rms_train = np.sqrt(mean_squared_error(e_dft_train, e_amp_train))
         # rms_valid_2 = np.sqrt(((e_amp_validation-e_dft_validation) ** 2).mean())
         # rms_train_2 = np.sqrt(((e_amp_train-e_dft_train) ** 2).mean())
         # print(rms_train_2, rms_valid_2)
-        plt.title('RMSE train=%s, valid=%s' % (rms_train, rms_valid))
+        plt.title('RMSE train=%s, valid=%s' % (np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)))
 
         plt.subplot(2, 1, 2)
         plt.hist(e_dft_train, color=colors[0], density=True, label='training')
-        plt.hist(e_dft_validation, color=colors[1], alpha=0.7, density=True, label='validation', ec='k')
-        plt.xlabel('DFT energy, eV')
-        plt.ylabel('Occurence frequency')
+        plt.hist(e_dft_valid, color=colors[1], alpha=0.7, density=True, label='validation', ec='k')
+        plt.xlabel('DFT energies, eV')
+        plt.ylabel('Occurrence frequency')
         plt.legend()
         plt.savefig('./' + fig_title + '.png')
-        return rms_train, rms_valid
+        return np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)
 
-    def fitting_force_plot(self, f_dft_train, f_dft_validation, f_amp_train, f_amp_validation, fig_title):
+    def fitting_force_plot(self, f_dft_train, f_dft_valid, f_amp_train, f_amp_valid, fig_title):
         """
         Summary:
             Plot the forces on x-y-z directions
-        Parameters:
-            train-valid dft/amp raw/rel forces for a specific force axis
         Returns:
-            Figure 1: fitting results
-            Figure 2: Histogram of forces sampling
+            Sub - Figure 1: fitting results
+                blue circles: raw DFT dataset
+                black line: fitted values
+                orange circles: predicted values
+            Sub - Figure 2: Histogram of forces sampling
         """
-        if self.force_option is False:
+        if not self.force_option:
             raise ValueError('Force_option is not turned on!')
-        else:
-            pass
 
         plt.figure(figsize=(6, 8))
         plt.subplot(2, 1, 1)
         plt.plot(f_dft_train, f_dft_train, '-k')
         plt.plot(f_dft_train, f_amp_train, 'o', fillstyle='full', color=colors[0],
                  label='training', markeredgecolor='k')
-        plt.plot(f_dft_validation, f_amp_validation, 'o', fillstyle='full', color=colors[1],
+        plt.plot(f_dft_valid, f_amp_valid, 'o', fillstyle='full', color=colors[1],
                  label='validation', markeredgecolor='k')
         plt.legend()
-        plt.xlabel('DFT force, eV/Ang')
-        plt.ylabel('ML-FF force, eV/Ang')
-        rms_valid = np.sqrt(mean_squared_error(f_dft_validation, f_amp_validation))
+        plt.xlabel('DFT forces, eV/Ang')
+        plt.ylabel('ML-FF forces, eV/Ang')
+        rms_valid = np.sqrt(mean_squared_error(f_dft_valid, f_amp_valid))
         rms_train = np.sqrt(mean_squared_error(f_dft_train, f_amp_train))
-        plt.title('RMSE train=%s, valid=%s' % (rms_train, rms_valid))
+        plt.title('RMSE train=%s, valid=%s' % (np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)))
 
         plt.subplot(2, 1, 2)
         plt.hist(f_dft_train, color=colors[0], density=True, label='training')
-        plt.hist(f_dft_validation, color=colors[1], alpha=0.7, density=True, label='validation', ec='k')
-        plt.xlabel('DFT force, eV/Ang')
-        plt.ylabel('Occurence frequency')
+        plt.hist(f_dft_valid, color=colors[1], alpha=0.7, density=True, label='validation', ec='k')
+        plt.xlabel('DFT forces, eV/Ang')
+        plt.ylabel('Occurrence frequency')
         plt.legend()
         plt.savefig('./' + fig_title + '.png')
-        return rms_train, rms_valid
+        return np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)
