e8a3dd1a1e5bd6db518d665648e4cf95678e4b6b
Chenghan Sun <sunchenghan1995@gmail.com>
2020-02-05 14:13:31 -0800

    test passed

--- src/training_utils.py
+++ src/training_utils.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 This file:
     Repo for utilities of Atomistic Machine Learning Project
@@ -125,11 +123,11 @@ class TrainTools:
             e_dft_train_dict: dictionary of DFT energies for training set
             e_dft_valid_dict：dictionary of DFT energies for validation set
         """
-        if self.force_option:
+        if not self.force_option:
             raise Exception("Force training is turned off for only energy training!")
 
         for index, atoms in enumerate(training_traj):
-            if not set_calc:  # training set & DFT
+            if not set_calc:  # if just DFT
                 e_dft = atoms.get_potential_energy()  # assign DFT calculator
                 self.e_train_dict[index] = e_dft  # update dictionary
             elif set_calc:  # training set & ML
@@ -150,9 +148,9 @@ class TrainTools:
             else:
                 raise Exception("Get Energies: validation set calculator set-up is not specified!")
 
-        if rel_option:
+        if not rel_option:
             return self.e_train_dict, self.e_valid_dict
-        elif not rel_option:
+        else:
             min_e_dft_train = min(self.e_train_dict.items(), key=lambda x: x[1])[1]  # minimum value in dictionary
             min_e_dft_valid = min(self.e_valid_dict.items(), key=lambda x: x[1])[1]
             min_e_dft = min(min_e_dft_train, min_e_dft_valid)  # ground minimum
@@ -204,7 +202,7 @@ class TrainTools:
                 raise Exception("Get Forces: validation set calculator set-up is not specified!")
         return self.f_train_dict, self.f_valid_dict
 
-    def force_decompose(self, rel_option=False, norm_option=False, normalize=False):
+    def force_decompose(self, normalize=False):
         """
         Summary:
             Decompose forces into X-Y-Z axises
@@ -222,6 +220,7 @@ class TrainTools:
             x_train_force_list.append(i[0])
             y_train_force_list.append(i[1])
             z_train_force_list.append(i[2])
+        # print(x_train_force_list)  # test
 
         x_valid_force_list = []
         y_valid_force_list = []
@@ -234,15 +233,16 @@ class TrainTools:
             z_valid_force_list.append(i[2])
 
         # normalize each decomposed forces
-        if not normalize:  # default normalize=False
-            x_nor_train_force_list = preprocessing.normalize(x_train_force_list, norm='l2')
-            y_nor_train_force_list = preprocessing.normalize(y_train_force_list, norm='l2')
-            z_nor_train_force_list = preprocessing.normalize(z_train_force_list, norm='l2')
-            x_nor_valid_force_list = preprocessing.normalize(x_valid_force_list, norm='l2')
-            y_nor_valid_force_list = preprocessing.normalize(y_valid_force_list, norm='l2')
-            z_nor_valid_force_list = preprocessing.normalize(z_valid_force_list, norm='l2')
-            return x_nor_train_force_list, x_nor_valid_force_list, y_nor_train_force_list, \
-                   y_nor_valid_force_list, z_nor_train_force_list, z_nor_valid_force_list
+        if normalize:  # default normalize=False
+            x_nor_train_force_list = preprocessing.normalize(np.array(x_train_force_list).reshape(1, -1), norm='l2')
+            y_nor_train_force_list = preprocessing.normalize(np.array(y_train_force_list).reshape(1, -1), norm='l2')
+            z_nor_train_force_list = preprocessing.normalize(np.array(z_train_force_list).reshape(1, -1), norm='l2')
+            x_nor_valid_force_list = preprocessing.normalize(np.array(x_valid_force_list).reshape(1, -1), norm='l2')
+            y_nor_valid_force_list = preprocessing.normalize(np.array(y_valid_force_list).reshape(1, -1), norm='l2')
+            z_nor_valid_force_list = preprocessing.normalize(np.array(z_valid_force_list).reshape(1, -1), norm='l2')
+            return x_nor_train_force_list.flatten(), x_nor_valid_force_list.flatten(), \
+                y_nor_train_force_list.flatten(), y_nor_valid_force_list.flatten(), \
+                z_nor_train_force_list.flatten(), z_nor_valid_force_list.flatten()
 
         else:
             return x_train_force_list, x_valid_force_list, y_train_force_list, y_valid_force_list, \
@@ -275,7 +275,7 @@ class TrainTools:
         # rms_valid_2 = np.sqrt(((e_amp_validation-e_dft_validation) ** 2).mean())
         # rms_train_2 = np.sqrt(((e_amp_train-e_dft_train) ** 2).mean())
         # print(rms_train_2, rms_valid_2)
-        plt.title('RMSE train=%s, valid=%s' % (np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)))
+        plt.title('RMSE train=%s, valid=%s' % (np.round(rms_train, decimals=3), np.round(rms_valid, decimals=3)))
 
         plt.subplot(2, 1, 2)
         plt.hist(e_dft_train, color=colors[0], density=True, label='training')
@@ -284,7 +284,7 @@ class TrainTools:
         plt.ylabel('Occurrence frequency')
         plt.legend()
         plt.savefig('./' + fig_title + '.png')
-        return np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)
+        return np.round(rms_train, decimals=3), np.round(rms_valid, decimals=3)
 
     def fitting_force_plot(self, f_dft_train, f_dft_valid, f_amp_train, f_amp_valid, fig_title):
         """
@@ -312,7 +312,7 @@ class TrainTools:
         plt.ylabel('ML-FF forces, eV/Ang')
         rms_valid = np.sqrt(mean_squared_error(f_dft_valid, f_amp_valid))
         rms_train = np.sqrt(mean_squared_error(f_dft_train, f_amp_train))
-        plt.title('RMSE train=%s, valid=%s' % (np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)))
+        plt.title('RMSE train=%s, valid=%s' % (np.round(rms_train, decimals=3), np.round(rms_valid, decimals=3)))
 
         plt.subplot(2, 1, 2)
         plt.hist(f_dft_train, color=colors[0], density=True, label='training')
@@ -321,4 +321,4 @@ class TrainTools:
         plt.ylabel('Occurrence frequency')
         plt.legend()
         plt.savefig('./' + fig_title + '.png')
-        return np.round(rms_train, decimals=2), np.round(rms_valid, decimals=2)
+        return np.round(rms_train, decimals=3), np.round(rms_valid, decimals=3)
